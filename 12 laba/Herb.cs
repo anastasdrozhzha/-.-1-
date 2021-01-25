8e7ed54ad519a72f04f679b68492dfc04abba97d
anastasdrozhzha <71096457+anastasdrozhzha@users.noreply.github.com>
2021-01-25 19:23:23 +0300

    Add files via upload

---
+++ 12 laba/Herb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace exersices_5
+{
+    public partial class Herb
+    {
+        public string Color;
+        public int Spike;
+        public int Number;
+        public int Sale;
+        const int LimitNumber = 10000;
+        public virtual string HerbCutting()
+        {
+            if (Number < LimitNumber)
+            {
+                return "Впринципе, вы можете вырубать эти деревья, но подумайте об экологии";
+            }
+            else return "Необходимо большее количество деревьев";
+        }
+        public void ShowString()
+        {
+            Console.WriteLine("All operations are completely");
+        }
+    }
+}
